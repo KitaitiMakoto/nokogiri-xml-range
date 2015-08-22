3334cd98c150cbd6ffcbceaff2bb02995a3dca85
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-23 05:32:06 +0900

    Indent

--- lib/nokogiri/xml/range/extension.rb
+++ lib/nokogiri/xml/range/extension.rb
@@ -1,17 +1,19 @@
-class Nokogiri::XML::Range
-  module Extension
-    refine Nokogiri::XML::Node do
-      def ancestors_to(node)
-        nodes = []
-        current = self
-        root = document.root
-        nodes.unshift current
-        until current == node or current == root
-          current = current.parent
+module Nokogiri::XML
+  class Range
+    module Extension
+      refine Nokogiri::XML::Node do
+        def ancestors_to(node)
+          nodes = []
+          current = self
+          root = document.root
           nodes.unshift current
+          until current == node or current == root
+            current = current.parent
+            nodes.unshift current
+          end
+          return unless current == node
+          nodes
         end
-        return unless current == node
-        nodes
       end
     end
   end
