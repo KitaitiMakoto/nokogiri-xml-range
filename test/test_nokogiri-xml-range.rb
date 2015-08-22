9ac92b2322217fecb8cdbb72d4e5cca8ea9a00af
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-23 02:16:46 +0900

    Remove unneccessary blank lines

--- test/test_nokogiri-xml-range.rb
+++ test/test_nokogiri-xml-range.rb
@@ -2,11 +2,9 @@ require 'helper'
 require 'nokogiri/xml/range'
 
 class TestNokogiriXMLRange < Test::Unit::TestCase
-
   def test_version
     version = Nokogiri::XML::Range.const_get('VERSION')
 
     assert !version.empty?, 'should have a VERSION constant'
   end
-
 end
