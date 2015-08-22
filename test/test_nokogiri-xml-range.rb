b100fac916697d9c91b203363b1cc68cd537cc1b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-23 01:50:58 +0900

    Modify name of test class

--- test/test_nokogiri-xml-range.rb
+++ test/test_nokogiri-xml-range.rb
@@ -1,7 +1,7 @@
 require 'helper'
 require 'nokogiri/xml/range'
 
-class TestNokogiri::XML::Range < Test::Unit::TestCase
+class TestNokogiriXMLRange < Test::Unit::TestCase
 
   def test_version
     version = Nokogiri::XML::Range.const_get('VERSION')
