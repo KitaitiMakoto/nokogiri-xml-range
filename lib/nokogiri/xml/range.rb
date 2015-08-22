7d8db14f83aac514f94865508d1c7a464f58355f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-23 04:52:41 +0900

    Change method name: Range.compare_boundary_points -> compare_points

--- lib/nokogiri/xml/range.rb
+++ lib/nokogiri/xml/range.rb
@@ -6,14 +6,14 @@ using Nokogiri::XML::Range::Extension
 
 class Nokogiri::XML::Range
   class << self
-    def compare_boundary_points(node1, offset1, node2, offset2)
+    def compare_points(node1, offset1, node2, offset2)
       return unless node1.document == node2.document
 
       case node1 <=> node2
       when 0
         offset1 <=> offset2
       when 1
-        compare_boundary_points(node2, offset2, node1, offset1) * -1
+        compare_points(node2, offset2, node1, offset1) * -1
       else
         ancestors = node2.ancestors_to(node1) # nil or [node1, child of node1, ..., parent of node2, node2]
         if ancestors
--- test/test_nokogiri-xml-range.rb
+++ test/test_nokogiri-xml-range.rb
@@ -8,17 +8,17 @@ class TestNokogiriXMLRange < Test::Unit::TestCase
     assert !version.empty?, 'should have a VERSION constant'
   end
 
-  def test_compare_boundary_points
+  def test_compare_points
     doc = Nokogiri.XML('<root><child>child 1</child><child>child 2</child></root>')
     children = doc.search('child')
     child1 = children[0]
     child2 = children[1]
 
-    assert_equal -1, Nokogiri::XML::Range.compare_boundary_points(child1, 0, child1, 1)
-    assert_equal 1, Nokogiri::XML::Range.compare_boundary_points(child2, 0, child1, 0)
-    assert_equal 0, Nokogiri::XML::Range.compare_boundary_points(child1, 1, child1, 1)
-    assert_equal -1, Nokogiri::XML::Range.compare_boundary_points(doc.root, 0, child1, 0)
-    assert_equal 1, Nokogiri::XML::Range.compare_boundary_points(doc.root, 2, child1, 1)
-    assert_equal -1, Nokogiri::XML::Range.compare_boundary_points(child1, 0, child2, 3)
+    assert_equal -1, Nokogiri::XML::Range.compare_points(child1, 0, child1, 1)
+    assert_equal 1, Nokogiri::XML::Range.compare_points(child2, 0, child1, 0)
+    assert_equal 0, Nokogiri::XML::Range.compare_points(child1, 1, child1, 1)
+    assert_equal -1, Nokogiri::XML::Range.compare_points(doc.root, 0, child1, 0)
+    assert_equal 1, Nokogiri::XML::Range.compare_points(doc.root, 2, child1, 1)
+    assert_equal -1, Nokogiri::XML::Range.compare_points(child1, 0, child2, 3)
   end
 end
