b96f8292ca12f13a64c88d5bd5aa05c8d20f822b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-23 04:26:14 +0900

    Refine Nokogiri::XML::Node to add #ancestors_to

---
+++ lib/nokogiri/xml/range/extension.rb
@@ -0,0 +1,18 @@
+class Nokogiri::XML::Range
+  module Extension
+    refine Nokogiri::XML::Node do
+      def ancestors_to(node)
+        nodes = []
+        current = self
+        root = document.root
+        nodes.unshift current
+        until current == node or current == root
+          current = current.parent
+          nodes.unshift current
+        end
+        return unless current == node
+        nodes
+      end
+    end
+  end
+end
