396b17be9ff16529cc173ce23c868a9116155c49
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-26 21:52:42 +0900

    Add test cases for Replacable

---
+++ test/test_nokogiri-xml-replacable.rb
@@ -0,0 +1,21 @@
+require 'helper'
+require 'nokogiri'
+require 'nokogiri/xml/range/extension'
+require 'nokogiri/xml/replacable'
+
+class TestNokogiriXMLReplacable < Test::Unit::TestCase
+  def setup
+    @doc = Nokogiri::XML::Document.new
+    @text = Nokogiri::XML::Text.new('Hello, world.', @doc)
+  end
+
+  def test_substring_data
+    assert_equal 'Hello, world.', @text.substring_data(0, 9999)
+    assert_equal 'world', @text.substring_data(7, 5)
+  end
+
+  def test_replace_data
+    @text.replace_data 7, 5, 'Nokogiri'
+    assert_equal 'Hello, Nokogiri.', @text.content
+  end
+end
