93faee1eac4ef6546d44c4ed740e19fbc2e838e0
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-08-30 00:46:22 +0900

    Add test cases for Node#length

---
+++ test/test_nokogiri-xml-range-refinement.rb
@@ -0,0 +1,37 @@
+require 'helper'
+require 'nokogiri'
+require 'nokogiri/xml/range/refinement'
+
+class TestNokogiriXMLRangeRefinement < Test::Unit::TestCase
+  using Nokogiri::XML::Range::Refinement
+
+  def setup
+    @doc = Nokogiri.XML(<<EOD)
+<root>
+  <parent>
+    <child>child 1</child>
+    <child>child 2</child>
+  </parent>
+</root>
+EOD
+    @root = @doc.root
+    @parent = @root.search('parent').first
+    @child1 = @parent.search('child')[0]
+    @child2 = @parent.search('child')[1]
+  end
+
+  data({
+    'root' => [:root, 3],
+    'parent' => [:parent, 5],
+    'child1' => [:child1, 1],
+  })
+  def test_element_length(data)
+    node_name, length = data
+    node = instance_variable_get("@#{node_name}")
+    assert_equal length, node.length
+  end
+
+  def test_text_length
+    assert_equal 7, @child1.children[0].length
+  end
+end
